da779bc24bb9223b2896644300bcd043b58397a4
tarikipekci <ytarik.ipekci@gmail.com>
2024-04-07 14:03:32 +0300

    Added -> Day-Night Cycle

--- Source/SeniorProject/Environment/DayNightCycleManager.cpp
+++ Source/SeniorProject/Environment/DayNightCycleManager.cpp
@@ -1,29 +1,30 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "DayNightCycleManager.h"
 
 #include "Engine/DirectionalLight.h"
+#include "Net/UnrealNetwork.h"
 
 // Sets default values
 ADayNightCycleManager::ADayNightCycleManager()
 {
-	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this actor to call Tick() every frame
 	PrimaryActorTick.bCanEverTick = true;
-}
 
-void ADayNightCycleManager::PerformDayNightCycle(float DeltaTime)
-{
-	if(LightSource)
-	{
-		LightSource->AddActorLocalRotation(FRotator(TurnRate * DeltaTime, 0, 0));
-	}
+	// Set default sun rotation speed
+	SunRotationSpeed = 10.0f;
+
+	// Ensure replication is enabled for relevant variables
+	bReplicates = true;
 }
 
 // Called when the game starts or when spawned
 void ADayNightCycleManager::BeginPlay()
 {
 	Super::BeginPlay();
+
+	if(GetLocalRole() == ROLE_Authority)
+	{
+		CurrentTimeInSeconds = 200.0f;
+	}
 }
 
 // Called every frame
@@ -31,5 +32,67 @@ void ADayNightCycleManager::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	PerformDayNightCycle(DeltaTime);
+	if(GetLocalRole() == ROLE_Authority)
+	{
+		// Update time
+		CurrentTimeInSeconds += DeltaTime;
+		UpdateSunState();
+	}
+}
+
+void ADayNightCycleManager::OnRep_CurrentTimeInSeconds()
+{
+	// Do nothing on clients when replicated
+	UpdateSunState();
+}
+
+void ADayNightCycleManager::OnRep_TimeOfDay()
+{
+	// Do nothing on clients when replicated
+}
+
+void ADayNightCycleManager::UpdateSunState()
+{
+	if(CurrentTimeInSeconds >= DayDurationInSeconds)
+	{
+		CurrentTimeInSeconds -= DayDurationInSeconds;
+	}
+
+	// Calculate time of the day
+	TimeOfDay = FMath::Fmod(CurrentTimeInSeconds / DayDurationInSeconds, 1.0f);
+
+	// Rotate sun
+	if(SunLight)
+	{
+		FRotator NewRotation = FRotator(TimeOfDay * 360.0f, 0.0f, 0.0f);
+		SunLight->SetActorRotation(NewRotation);
+
+		// Replicate sun rotation to all clients if we are a listen server
+		if(GetNetMode() == NM_ListenServer)
+		{
+			SetSunRotation(NewRotation);
+		}
+	}
+}
+
+void ADayNightCycleManager::SetSunRotation_Implementation(const FRotator& NewRotation)
+{
+	if(SunLight)
+	{
+		SunLight->SetActorRotation(NewRotation);
+	}
+}
+
+bool ADayNightCycleManager::SetSunRotation_Validate(const FRotator& NewRotation)
+{
+	return true;
+}
+
+void ADayNightCycleManager::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	// Replicate these properties to all clients
+	DOREPLIFETIME_CONDITION(ADayNightCycleManager, CurrentTimeInSeconds, COND_None);
+	DOREPLIFETIME_CONDITION(ADayNightCycleManager, TimeOfDay, COND_None);
 }
--- Source/SeniorProject/Environment/DayNightCycleManager.h
+++ Source/SeniorProject/Environment/DayNightCycleManager.h
@@ -1,4 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
 #pragma once
 
 #include "CoreMinimal.h"
@@ -10,25 +9,52 @@ class ADirectionalLight;
 UCLASS()
 class SENIORPROJECT_API ADayNightCycleManager : public AActor
 {
-	GENERATED_BODY()
-	
-public:	
-	// Sets default values for this actor's properties
-	ADayNightCycleManager();
+    GENERATED_BODY()
 
-	void PerformDayNightCycle(float DeltaTime);
+public: 
+    // Sets default values for this actor's properties
+    ADayNightCycleManager();
 
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+    // Called when the game starts or when spawned
+    virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+public: 
+    // Called every frame
+    virtual void Tick(float DeltaTime) override;
 
-	UPROPERTY(EditAnywhere, Category = "Sky")
-	ADirectionalLight* LightSource;
+    // The total duration of a full day in seconds
+    UPROPERTY(EditAnywhere, Category = "Day Night Cycle")
+    float DayDurationInSeconds;
 
-	UPROPERTY(EditAnywhere, Category = "Sky")
-	float TurnRate;
+    // Current time in seconds
+    UPROPERTY(BlueprintReadOnly, ReplicatedUsing=OnRep_CurrentTimeInSeconds, Category = "Day Night Cycle")
+    float CurrentTimeInSeconds;
+
+    // Current time of the day (0-1 where 0 is midnight, 0.5 is noon, 1 is midnight again)
+    UPROPERTY(BlueprintReadOnly, ReplicatedUsing=OnRep_TimeOfDay, Category = "Day Night Cycle")
+    float TimeOfDay;
+
+    // Sun actor to rotate
+    UPROPERTY(EditAnywhere, Category = "Day Night Cycle")
+    ADirectionalLight* SunLight;
+
+    // Sun rotation speed
+    UPROPERTY(EditAnywhere, Category = "Day Night Cycle")
+    float SunRotationSpeed;
+
+    // Replicated function to update CurrentTimeInSeconds on clients
+    UFUNCTION()
+    void OnRep_CurrentTimeInSeconds();
+
+    // Replicated function to update TimeOfDay on clients
+    UFUNCTION()
+    void OnRep_TimeOfDay();
+
+    // Replicated function to set sun rotation on clients
+    UFUNCTION(BlueprintCallable, Server, Reliable, WithValidation)
+    void SetSunRotation(const FRotator& NewRotation);
+
+    UFUNCTION()
+    void UpdateSunState();
 };
